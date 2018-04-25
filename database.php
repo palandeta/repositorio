c16f4e3a247b6234a91acf3b1872fee65f8eaf83
palandeta <palandeta@utn.edu.ec>
2018-04-25 08:49:16 -0500

    Actualizo otra vez la conexión a bdd
    Estaba incompleta la línea de conexión

--- database.php
+++ database.php
@@ -28,7 +28,7 @@ class Database
           //self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);  
 	
 	//AZURE 25-04-18
-	    self::$cont =  new PDO("sqlsrv:server = tcp:servidordata.database.windows.net,1433; Database = bdd1", "pablo", "Alison2013
+	    self::$cont =  new PDO("sqlsrv:server = tcp:servidordata.database.windows.net,1433; Database = bdd1", "pablo", "Alison2013");
         }
         catch(PDOException $e) 
         {
