92b2a94eddce32a912dc01a4a29b38326436ba13
palandeta <palandeta@utn.edu.ec>
2018-05-03 12:34:08 -0500

    9no
    noveno

--- database.php
+++ database.php
@@ -5,10 +5,16 @@ class Database
 	//private static $dbHost = 'mysql23669-pablo.jl.serv.net.mx' ;
 	//private static $dbUsername = 'root';
 	//private static $dbUserPassword = 'ATCoch59681';
+/*
 	private static $dbName = 'sampledb' ; 
 	private static $dbHost = '10.129.29.12' ;
 	private static $dbUsername = 'root';
 	private static $dbUserPassword = 'LIHUFfRTDD55mbv5';
+*/
+	private static $dbName = 'bdd1' ; 
+	private static $dbHost = 'svrpg.postgres.database.azure.com' ;
+	private static $dbUsername = 'pablolandetalopez@svrpg';
+	private static $dbUserPassword = 'Pablolandeta666';
 	
 	
 	private static $cont  = null;
@@ -30,7 +36,7 @@ class Database
 	//AZURE 25-04-18
 	 //   self::$cont =  new PDO("sqlsrv:server = tcp:servidordata.database.windows.net,1433; Database = bdd1", "pablo", "Alison2013");
 	//Azure Postgres 03-05-18
-	 self::$cont =  new PDO("host=svrpg.postgres.database.azure.com port=5432 dbname=bdd1 user=pablolandetalopez@svrpg password=Pablolandeta666 sslmode=required");
+	 self::$cont =  new PDO("host=".self::$dbHost." port=5432 dbname=".self::$dbName." user=".self::$dbUsername." password=".self::$dbUserPassword." sslmode=required");
 	//self::$cont = new PDO('pgsql:dbname=bdd1;host=svrpg.postgres.database.azure.com;user=pablolandetalopez@svrpg;password=Pablolandeta666');		
         }
         catch(PDOException $e) 
